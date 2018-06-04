8964e4f6ca08aece2f3baeb4a9343f6046b107d6
Jason Bell <jason@mastodonc.com>
2018-06-04 15:36:20 +0100

    Added server startup script and minor changes to Dockerfile.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-JAR_LOCATION=${1:-$JAR_LOCATION}
-
+JAR_LOCATION=/srv
+echo "Starting kixi.paloma"
 exec java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -XX:ErrorFile=/tmp/hs_err_pid_%p.log -XX:+UseG1GC -Xloggc:/tmp/gc_%p.log -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=2M -XX:+PrintGCDateStamps ${JAVA_OPTS:-} -jar $JAR_LOCATION/server.jar >&1
